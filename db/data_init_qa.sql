297d94e8ffbfffdec6fd2ca9658e6f16d1bc059f
Shin <dongmin.shin@narvar.com>
2018-06-05 14:48:57 -0700

    Added test data

---
+++ db/data_init_qa.sql
@@ -0,0 +1,2 @@
+insert into book(title, author, year, pages) values ('Agile for Teams', 'cprime', 2015, 275);
+insert into book(title, author, year, pages) values ('Camel Crush Methol Silver', 'RJReynolds', 2017, 20);
